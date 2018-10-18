7e2d4f7e9099baddbc9c17d588ce2b41046ce57a
Chathura Hettiarachchi <chathura93@yahoo.com>
2018-10-18 10:35:30 +0530

    Custom routes for portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]  # removed show path and re-route
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' # need to change the index action too, adding 'as' define new prefix
 
   get 'about-me', to: 'pages#about' # can change name to any because it have path net to it
   get 'contact', to: 'pages#contact'
