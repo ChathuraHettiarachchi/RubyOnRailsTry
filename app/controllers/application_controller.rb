5d33cb891ad4f81016fba37114d3d8482136db26
Chathura Hettiarachchi <chathura93@yahoo.com>
2018-10-19 13:09:50 +0530

    Implement edit user page with new attribute name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@
 
 # Application controller
 class ApplicationController < ActionController::Base
+  before_action :configure_permited_parameters, if: :devise_controller?
+
+  def configure_permited_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:accout_update, keys: [:name])
+  end
 end
