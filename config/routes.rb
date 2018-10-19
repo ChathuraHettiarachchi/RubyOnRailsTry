4ac90c9c24515241fa6d98b304c8046d46504fce
Chathura Hettiarachchi <chathura93@yahoo.com>
2018-10-19 12:40:27 +0530

    Customize registration routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+  
   resources :portfolios, except: [:show]  # removed show path and re-route
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' # need to change the index action too, adding 'as' define new prefix
   get 'angular-items', to: 'portfolios#angular'
