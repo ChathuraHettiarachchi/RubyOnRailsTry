3b6ed29059ff2ecf0ff0c2136c2976f162c1e4e8
Chathura Hettiarachchi <chathura93@yahoo.com>
2018-10-19 12:36:29 +0530

    Added basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]  # removed show path and re-route
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' # need to change the index action too, adding 'as' define new prefix
   get 'angular-items', to: 'portfolios#angular'
